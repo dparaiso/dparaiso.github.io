8fa640f51af2f9803cbea58896eb50fbd435da17
dparaiso <danieva.paraiso@gmail.com>
2024-07-27 20:38:14 -0700

    update toggles in project section

--- js/button.js
+++ js/button.js
@@ -12,3 +12,13 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+let myLabels = document.querySelectorAll('.lbl-toggle'); 
+Array.from(myLabels).forEach(label => 
+  { label.addEventListener('keydown', e => 
+    { if (e.which === 32 || e.which === 13)
+       { e.preventDefault(); label.click();
+
+        }; 
+      });
+    }); 
